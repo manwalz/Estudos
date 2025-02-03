12db8af643d60d5b943c1f5022422b1c443b768e
manwalz <138518411+manwalz@users.noreply.github.com>
2025-02-02 23:04:55 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,239 @@
+const studiesData = {
+  "data": [
+    {
+      "Estudo": "LOTIS-5",
+      "Doenca": "LLA",
+      "Fase": 1.0,
+      "Idade": 10,
+      "Exclusao": [
+        "Dengue"
+      ],
+      "Tratamento": [],
+      "Local": [
+        "Dourados"
+      ],
+      "Droga": [
+        "Dipirona"
+      ]
+    },
+    {
+      "Estudo": "ACRUE",
+      "Doenca": "LDGCB",
+      "Fase": 2.0,
+      "Idade": 65,
+      "Exclusao": [
+        "HAS não controlada",
+        "Doença hemorrágica",
+        "Uso de anticoagulante",
+        "Transplante renal",
+        "Cirurgia bariátrica",
+        "DRGE",
+        "Má absorção intestinal",
+        "Câncer prévio ou atual",
+        "Hepatites",
+        "HIV",
+        "Acometimento do SNC",
+        "CIRS-G com pontuação única de 4",
+        "CIRS-G > 6",
+        "AVC nos últimos 6 meses",
+        "Doença cardiovascular significativa",
+        "Arritmias (exceto FA controlada)",
+        "Cirurgia de grande porte nos últimos 30 dias",
+        "Uso de inibidor potente de CYP3A",
+        "Vacina de vírus vivo nos últimos 30 dias",
+        "Participação em outro estudo clínico"
+      ],
+      "Tratamento": [
+        "1L"
+      ],
+      "Local": [
+        "Brasília",
+        "Pernambuco"
+      ],
+      "Droga": [
+        "Acalabrutinibe",
+        "Rituximabe em braço único"
+      ]
+    },
+    {
+      "Estudo": "GOLSEEK-1",
+      "Doenca": "LDGCB",
+      "Fase": 3.0,
+      "Idade": "Qualquer idade",
+      "Exclusao": [
+        "Ann Arbor I",
+        "Infecção ativa",
+        "Doença psiquiátrica",
+        "Acometimento do SNC"
+      ],
+      "Tratamento": [
+        "1L"
+      ],
+      "Local": [
+        "AC Camargo",
+        "SJRP",
+        "Chapecó",
+        "Jaú",
+        "9 de Julho",
+        "NOHC Ceará"
+      ],
+      "Droga": [
+        "Golcadomida",
+        "RCHOP"
+      ]
+    },
+    {
+      "Estudo": "LINKER-MM3",
+      "Doenca": "Mieloma Múltiplo",
+      "Fase": 3.0,
+      "Idade": "Qualquer idade",
+      "Exclusao": [
+        "Tratamento prévio com elotozumabe ou pomalidomida",
+        "Tratamento prévio com anti-BCMA",
+        "Tratamento prévio com CAR-T",
+        "Acometimento do SNC",
+        "Outras discrasias plasmocitárias",
+        "Alogênico nos últimos 6 meses",
+        "Autólogo nos últimos 3 meses",
+        "Doença cardiovascular significativa",
+        "Doença renal significativa",
+        "Doença hepática significativa"
+      ],
+      "Tratamento": [
+        "Obrigatório lenalidomida e inibidor de proteassoma"
+      ],
+      "Local": [
+        "AC Camargo",
+        "Clínica São Germano"
+      ],
+      "Droga": [
+        "Linvoseltamabe x Elotuzumabe",
+        "Pomalidomida"
+      ]
+    },
+    {
+      "Estudo": "MAHOGANY",
+      "Doenca": "Linfoma Folicular\nLinfoma Marginal",
+      "Fase": 3.0,
+      "Idade": "Qualquer idade",
+      "Exclusao": [
+        "Doença não mensurável por imagem",
+        "Doença cardiovascular significativa",
+        "Câncer prévio ou atual",
+        "Infecção ativa"
+      ],
+      "Tratamento": [
+        "Obrigatório anti-CD20"
+      ],
+      "Local": [
+        "Einstein",
+        "UNESP",
+        "Campinas",
+        "Pernambuco",
+        "Salvador",
+        "Porto Alegre",
+        "Curitiba",
+        "Vitória",
+        "Blumenau",
+        "USP",
+        "Barretos",
+        "Niterói",
+        "Rondônia",
+        "Rio de Janeiro",
+        "Brasília"
+      ],
+      "Droga": [
+        "Zanubrutinibe",
+        "Anti-CD20 x Lenalidomida",
+        "Rituximabe"
+      ]
+    },
+    {
+      "Estudo": "ELEMENT MDS",
+      "Doenca": "Mielodisplasia",
+      "Fase": NaN,
+      "Idade": NaN,
+      "Exclusao": [],
+      "Tratamento": [],
+      "Local": [],
+      "Droga": []
+    }
+  ],
+  "exclusionCriteria": [
+    "AVC nos últimos 6 meses",
+    "Acometimento do SNC",
+    "Alogênico nos últimos 6 meses",
+    "Ann Arbor I",
+    "Arritmias (exceto FA controlada)",
+    "Autólogo nos últimos 3 meses",
+    "CIRS-G > 6",
+    "CIRS-G com pontuação única de 4",
+    "Cirurgia bariátrica",
+    "Cirurgia de grande porte nos últimos 30 dias",
+    "Câncer prévio ou atual",
+    "DRGE",
+    "Dengue",
+    "Doença cardiovascular significativa",
+    "Doença hemorrágica",
+    "Doença hepática significativa",
+    "Doença não mensurável por imagem",
+    "Doença psiquiátrica",
+    "Doença renal significativa",
+    "HAS não controlada",
+    "HIV",
+    "Hepatites",
+    "Infecção ativa",
+    "Má absorção intestinal",
+    "Outras discrasias plasmocitárias",
+    "Participação em outro estudo clínico",
+    "Transplante renal",
+    "Tratamento prévio com CAR-T",
+    "Tratamento prévio com anti-BCMA",
+    "Tratamento prévio com elotozumabe ou pomalidomida",
+    "Uso de anticoagulante",
+    "Uso de inibidor potente de CYP3A",
+    "Vacina de vírus vivo nos últimos 30 dias"
+  ],
+  "locations": [
+    "9 de Julho",
+    "AC Camargo",
+    "Barretos",
+    "Blumenau",
+    "Brasília",
+    "Campinas",
+    "Chapecó",
+    "Clínica São Germano",
+    "Curitiba",
+    "Dourados",
+    "Einstein",
+    "Jaú",
+    "NOHC Ceará",
+    "Niterói",
+    "Pernambuco",
+    "Porto Alegre",
+    "Rio de Janeiro",
+    "Rondônia",
+    "SJRP",
+    "Salvador",
+    "UNESP",
+    "USP",
+    "Vitória"
+  ],
+  "drugs": [
+    "Acalabrutinibe",
+    "Anti-CD20 x Lenalidomida",
+    "Dipirona",
+    "Golcadomida",
+    "Linvoseltamabe x Elotuzumabe",
+    "Pomalidomida",
+    "RCHOP",
+    "Rituximabe",
+    "Rituximabe em braço único",
+    "Zanubrutinibe"
+  ],
+  "treatments": [
+    "1L",
+    "Obrigatório anti-CD20",
+    "Obrigatório lenalidomida e inibidor de proteassoma"
+  ]
+};
